d186d8ab7a59677f195dcfeed904ca29153bac97
mohammadkazem vaziri <mohkazv@hotmail.com>
2021-01-13 11:00:49 +0330

    some Updates

---
+++ Library/MemberDashboard.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Library
+{
+    /// <summary>
+    /// Interaction logic for MemberDashboard.xaml
+    /// </summary>
+    public partial class MemberDashboard : Window
+    {
+        public MemberDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BorrowedBookBtn_Click(object sender, RoutedEventArgs e)
+        {
+            new MemberborrowedBook().Show();
+            Close();
+        }
+
+        private void AccountDashboardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            new MemberAccountDashboard().Show();
+            Close();
+        }
+
+        private void LogoutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Library/MemberborrowedBook.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Library
+{
+    /// <summary>
+    /// Interaction logic for MemberborrowedBook.xaml
+    /// </summary>
+    public partial class MemberborrowedBook : Window
+    {
+        public MemberborrowedBook()
+        {
+            InitializeComponent();
+        }
+
+        private void ExtendDateBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            new MemberDashboard().Show();
+                Close();
+        }
+    }
+}
